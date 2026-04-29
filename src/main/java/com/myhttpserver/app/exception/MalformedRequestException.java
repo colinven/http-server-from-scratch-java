8a53da3c600458dfd182c6a75158e12a58e6a190
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-04-29 16:44:13 -0400

    rm MalformedRequestException

--- src/main/java/com/myhttpserver/app/exception/MalformedRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.myhttpserver.app.exception;
-
-public class MalformedRequestException extends RuntimeException {
-  public MalformedRequestException(String message) {
-    super(message);
-  }
-}
